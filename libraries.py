456d93f4cc9eb238fdb8213f31eddf4b8fe248ee
Alex_Arokiaraj <alexantony.arokiaraj@gmail.com>
2025-04-21 23:04:53 +0200

    before running new beta

--- libraries.py
+++ libraries.py
@@ -22,23 +22,23 @@ class DataArray(Enum):
 
 class RunParameters(Enum):
     sim = True
-    plots = True
+    plots = False
     debug=False
     final_plots_flag = False
     save_final_data=False
     with_missing_pads = True
-    batch_mode = False
-    save_to_root = False
+    batch_mode = True
+    save_to_root = True
     save_python_figures = False
     optimize_alpha = False
-    optimize_beta = False
+    optimize_beta = True
     optimize_gamma = False
-    optimize_cdist = True
+    optimize_cdist = False
     optimize_pij = False
-    optimize_multiplicity = True
-    save_root_file = "/mnt/ksf2/H1/user/u0100486/linux/doctorate/github/tracker_new/output/optimize/test/"
-    tag = "mul"
-    exc_file_name = "/mnt/ksf2/H1/user/u0100486/linux/doctorate/github/tracker_new/output/optimize/test/"
+    optimize_multiplicity = False
+    save_root_file = "/mnt/ksf2/H1/user/u0100486/linux/doctorate/github/tracker_new/output/optimize/beta_single_new/"
+    tag = "beta"
+    exc_file_name = "/mnt/ksf2/H1/user/u0100486/linux/doctorate/github/tracker_new/output/optimize/beta_single_new/"
     range_lookup_table = "/mnt/ksf2/H1/user/u0100486/linux/doctorate/github/tracker_new/LookupTable_e780_58Ni_68Ni_Alex.xlsx"
     range_energy_conversion_sheet = "range_energy_he_he_cf4_mixed"
 
